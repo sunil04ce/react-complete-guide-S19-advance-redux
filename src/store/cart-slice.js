7d90494e1a89b055c59e9eecff7834fdc1c99f44
sunil parmar <sunil04ce@gmail.com>
2023-03-01 22:23:20 +0530

    add remove item from cart

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -10,9 +10,12 @@ const cartSlice = createSlice({
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
+
+      state.totalQantity++;
+
       if (!existingItem) {
         state.items.push({
-          itemId: newItem.id,
+          id: newItem.id,
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
@@ -27,6 +30,9 @@ const cartSlice = createSlice({
     removeItemFromCart(state, action) {
       const id = action.payload;
       const existingItem = state.items.find((item) => item.id === id);
+
+      state.totalQantity--;
+
       if (existingItem.quantity === 1) {
         state.items = state.items.filter((item) => item.id !== id);
       } else {
